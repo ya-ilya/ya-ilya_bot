433898c0901b102a7ba688814e3042c122d8e90b
ya-ilya <69980831+ya-ilya@users.noreply.github.com>
2020-11-07 11:39:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const client = new Discord.Client();
+const config = require('./config.json')
+client.commands = new Discord.Collection()
+
+fs.readdir('./commands', (err, files) => {
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split('.').pop() === 'js')
+    if (jsfile.length <= 0) return console.log('Commands not found')
+
+    console.log(`Loading commands...`)
+    jsfile.forEach((f, i) => {
+        let props = require(`./commands/${f}`)
+        client.commands.set(props.help.name, props)
+    })
+})
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  let prefix = config.prefix
+  let messageArray = msg.content.split(' ')
+  let command = messageArray[0]
+  let args = messageArray.slice(1)
+
+  let command_file = client.commands.get(command.slice(prefix.length))
+  if (command_file) command_file.run(client, msg, args)
+
+  if (command === 'help commands') {
+    client.commands.get('help-cmd').execute(message, args)
+  }
+  if (msg.content.startsWith(prefix + "ping")) {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('TOKEN-HERE'); //bot token
